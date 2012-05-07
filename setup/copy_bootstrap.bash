9a0e266e28c8153b5ab5aee8a565bcd6e351c0c8
William Metachu Chu <chudood@gmail.com>
2012-05-08 09:28:17 +1000

    Updated Bootstrap

--- setup/copy_bootstrap.bash
+++ setup/copy_bootstrap.bash
@@ -15,7 +15,7 @@ STATIC_DIR="$SITE_CODE_DIR/$APP_NAME/static"
 TEMPLATE_DIR="$SITE_CODE_DIR/$APP_NAME/templates"
 PUBLIC_DIR="$SITE_CODE_DIR/../../public"
 BOOTSTRAP_URL="http://twitter.github.com/bootstrap/assets/bootstrap.zip"
-
+JQUERY_URL="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"
 ## Main ##
 
 cd $SITE_CODE_DIR
@@ -23,7 +23,7 @@ cd $SITE_CODE_DIR
 rm -rf $BOOTSTRAP_DIR
 mkdir $BOOTSTRAP_DIR
 
-wget BOOTSTRAP_URL -O /tmp/bootstrap.zip
+wget $BOOTSTRAP_URL -O /tmp/bootstrap.zip
 unzip /tmp/bootstrap.zip -d $SITE_CODE_DIR
 
 cp $BOOTSTRAP_DIR/apple-touch-icon.png $STATIC_DIR/apple-touch-icon.png
@@ -31,10 +31,10 @@ cp $BOOTSTRAP_DIR/favicon.ico $STATIC_DIR/favicon.ico
 
 mkdir -p $STATIC_DIR/js
 
+wget $JQUERY_URL -O $BOOTSTRAP_DIR/js
 cp -r $BOOTSTRAP_DIR/js/*.js $STATIC_DIR/js/
 
 mkdir -p $STATIC_DIR/css
 cp $BOOTSTRAP_DIR/css/*.css $STATIC_DIR/css/
-
 
 
